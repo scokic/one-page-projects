e4dae26b03ea9bebb5c526333e571c77b5c3cf16
scokic <strahinjacokic@gmail.com>
2021-04-18 09:00:47 +0200

    bn

--- projects/Simple Todo App/js/script.js
+++ projects/Simple Todo App/js/script.js
@@ -1,34 +1,106 @@
-// Tasks
-let tasks = [{ taskName: "New task", taskStatus: false }];
+"use strict";
+
+let tasks = [
+  { taskName: "New task", taskStatus: false, taskId: 1 },
+  { taskName: "New task 2", taskStatus: true, taskId: 2 },
+  { taskName: "New task 3", taskStatus: true, taskId: 3 },
+];
 
 let taskList = document.querySelector(".task-list");
+
+let taskEditButton = document.querySelector(".edit-button");
 let newTaskName = document.querySelector("#new-task-name");
 let newTaskForm = document.querySelector(".new-task-form");
 
-function newTaskSubmit() {
-  let newTask = { taskName: newTaskName.value, taskStatus: false };
-  tasks.push(newTask);
-  console.log(newTask);
+/* ------------------------------ ADD NEW TASK ------------------------------ */
 
-  let createNewTask = document.createElement("div");
-  createNewTask.classList.add("task");
+function createNewTask() {
+  if (newTaskName.value === "") {
+    alert("pls fix");
+  } else {
+    let newTask = { taskName: newTaskName.value, taskStatus: false };
+    tasks.push(newTask);
 
-  let createNewTaskCheckbox = document.createElement("input");
-  createNewTaskCheckbox.type = "checkbox";
-  createNewTask.appendChild(createNewTaskCheckbox);
+    taskList.insertAdjacentHTML(
+      "beforeEnd",
+      `
+        <div class="task">
+                <input type="checkbox"/>
+                <label>${newTaskName.value}</label>
+                <div class="delete-button">
+                  <i class="fas fa-times"></i>
+                </div>
+              </div>
+        `
+    );
 
-  let createNewTaskName = document.createElement("label");
-  createNewTaskName.textContent = newTaskName.value;
-  createNewTask.appendChild(createNewTaskName);
+    newTaskName.value = "";
+    newTaskName.focus();
+  }
 
-  taskList.appendChild(createNewTask);
-
-  // Zaustavljanje automatskog refresh-a nakon submit-a
+  //Zaustavljanje automatskog refresh-a nakon submit-a
   function handleForm(event) {
     event.preventDefault();
   }
   newTaskForm.addEventListener("submit", handleForm);
-
-  newTaskName.value = "";
-  newTaskName.focus();
 }
+
+/* ------------------------------ READ EXISTING TASKS ------------------------------ */
+
+function printTasks() {
+  let i;
+  for (i = 0; i < tasks.length; i++) {
+    if (tasks[i].taskStatus === true) {
+      taskList.insertAdjacentHTML(
+        "beforeEnd",
+        `
+          <div class="task">
+                  <input type="checkbox" class="task-status" checked/>
+                  <label>${tasks[i].taskName}</label>
+                  <div class="delete-button">
+                    <i class="fas fa-times"></i>
+                  </div>
+                </div>
+          `
+      );
+    } else {
+      taskList.insertAdjacentHTML(
+        "beforeEnd",
+        `
+          <div class="task">
+                  <input type="checkbox" class="task-status" />
+                  <label>${tasks[i].taskName}</label>
+                  <div class="delete-button">
+                    <i class="fas fa-times"></i>
+                  </div>
+                </div>
+          `
+      );
+    }
+  }
+}
+
+printTasks();
+
+/* ------------------------------ TOGGLE EDIT BUTTON ------------------------------ */
+
+// prikazivanje dugmeta za brisanje taskova
+taskEditButton.addEventListener("click", () => {
+  if (taskList.classList.contains("hidden")) {
+    taskList.classList.remove("hidden");
+    taskEditButton.textContent = "CANCEL";
+    taskEditButton.style.backgroundColor = "#505767";
+  } else {
+    taskList.classList.add("hidden");
+    taskEditButton.textContent = "EDIT";
+    taskEditButton.style.backgroundColor = "#ef7157";
+  }
+});
+
+/* ------------------------------ DELETE A TASK ------------------------------ */
+
+// selektuj task
+// obriši task sa fronta
+// obriši task iz liste svih taskova
+
+/* ------------------------------ MARK TASK AS DONE ------------------------------ */
